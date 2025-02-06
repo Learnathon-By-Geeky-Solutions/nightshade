53399c0d0ad606ea1cea6a0afc978bef2b998a6d
MD. Nafiz Ahmed <nafizahmed5420@gmail.com>
2025-02-06 20:43:46 +0600

    Player feature update

---
+++ Assets/PlayerDashState.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDashState : PlayerState
+{
+    public PlayerDashState(Player _player, PlayerStateMachine _stateMachine, string _animBoolName) : base(_player, _stateMachine, _animBoolName)
+    {
+    }
+
+    // Start is called before the first frame update
+    public override void Enter()
+    {
+        base.Enter();
+        stateTimer = player.dashDuration;
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (!player.IsGroundDetected() && player.IsWallDetected())
+            stateMachine.ChangeState(player.wallSlide);
+
+        player.SetVelocity(player.dashSpeed * player.dashDir, 0);
+        if (stateTimer < 0)
+            stateMachine.ChangeState(player.idleState);
+    }
+}
