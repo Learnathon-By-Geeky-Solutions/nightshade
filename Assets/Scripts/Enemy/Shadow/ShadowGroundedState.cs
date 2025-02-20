e02fa2f5c07bff1c2c7279b9a6f4e29b1be26327
MD. Nafiz Ahmed <100011710+Shaneomax@users.noreply.github.com>
2025-02-21 01:34:27 +0600

    enemy update

---
+++ Assets/Scripts/Enemy/Shadow/ShadowGroundedState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowGroundedState : EnemyState
+{
+    protected Enemy_Shadow enemy;
+
+    public ShadowGroundedState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Shadow enemy)
+        : base(_enemyBase, _stateMachine, _animBoolName)
+    {
+        this.enemy = enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if(enemy.IsPlayerDetected())
+            stateMachine.ChangeState(enemy.battleState);
+    }
+}
---
+++ Assets/Scripts/Enemy/Shadow/ShadowStunnedState.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowStunnedState : EnemyState
+{
+    private Enemy_Shadow enemy;
+
+    public ShadowStunnedState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Shadow _enemy) : base(_enemyBase, _stateMachine, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+
+        enemy.fx.InvokeRepeating("RedColorBlink", 0, .1f);
+
+        stateTimer = enemy.stunDuration;
+
+        rb.velocity = new Vector2(-enemy.facingDir * enemy.stunDirection.x, enemy.stunDirection.y);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+
+        enemy.fx.Invoke("CancelRedBlink", 0);
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (stateTimer < 0)
+            stateMachine.ChangeState(enemy.idleState);
+    }
+}
